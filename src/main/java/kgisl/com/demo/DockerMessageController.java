6f2c4a16da59576a4243b08d9e69bf8cf5e52634
Suneeti P N <suneeti.pn@kgisl.com>
2024-10-21 15:02:38 +0530

    latest

---
+++ src/main/java/kgisl/com/demo/DockerMessageController.java
@@ -0,0 +1,12 @@
+package kgisl.com.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerMessageController {
+    @GetMapping("/messages")
+    public String getMessage() {
+        return "Hello from Docker!";
+    }
+}
